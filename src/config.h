8e056a439577438a6cf746f7863528b5a683e78b
CaledoniaProject <the.warl0ck.1989@gmail.com>
2013-07-11 13:41:52 +0800

    Let us release that way

--- src/config.h
+++ src/config.h
@@ -1,6 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define TC_DEFAULT_DOWNLOAD_TEMPLATE "aria2c -c --header='Cookies: gdriveid=%1' -o '%2' '%3'"
+#define TC_DEFAULT_DOWNLOAD_TEMPLATE "aria2c -c --header='Cookie: gdriveid=%1' -o '%2' '%3'"
 
 #endif // CONFIG_H
