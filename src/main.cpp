38ae12e3966eb95ce6ba08603e8cdd2a3d49ddcc
CaledoniaProject <the.warl0ck.1989@gmail.com>
2013-12-20 13:14:13 +0800

    Let us release that way

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationName("CloudClient");
-    a.setApplicationVersion("0.68");
+    a.setApplicationVersion("0.69");
     a.setOrganizationName("Labo-A.L");
 
     Util::init();
